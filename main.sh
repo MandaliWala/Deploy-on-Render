587984ae20e3f58fb49838ec7e92a0c1abdc0bdd
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-12-26 10:36:55 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,24 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Clone karne ke liye UPSTREAM_REPO mai repo url toh do !!!! "
+  exit 1
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /N2Bot
+fi
+
+# Upgrade pip and install requirements
+cd N2Bot
+pip install -U -r requirements.txt
+
+# Start bot
+echo "Starting Bot....✨"
+
+if [ -z $START_CMD ]
+then
+  echo "koi START_CMD nahin diya hai isiliye python3 bot.py run ho raha hai"
+  python3 bot.py
+else
+  echo "running $START_CMD "
+  $START_CMD
+fi
